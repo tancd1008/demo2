3d1534f04b3e10c9298f10845028ce093d4c6383
Tancd <caotan1008@gmail.com>
2023-07-03 18:59:22 +0900

    tailwin css

--- src/pages/client/Home.js
+++ src/pages/client/Home.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
-    <div>HomePage</div>
-  )
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
