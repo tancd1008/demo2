f81647a4d3ecbff86353e9117062637978978bae
Tancd <caotan1008@gmail.com>
2023-07-08 13:32:49 +0900

    update cart

--- src/components/card/ItemProductCart.js
+++ src/components/card/ItemProductCart.js
@@ -1,30 +1,76 @@
-import React from 'react'
+import React from "react";
 
 const ItemProductCart = () => {
+  const productsCart = [
+    {
+      id: 1,
+      name: "Basic Tee",
+      href: "#",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: "$35",
+      color: "Black",
+    },
+    {
+      id: 2,
+      name: "Basic Tee",
+      href: "#",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: "$35",
+      color: "Black",
+    },
+    {
+      id: 3,
+      name: "Basic Tee",
+      href: "#",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: "$35",
+      color: "Black",
+    },{
+      id: 4,
+      name: "Basic Tee",
+      href: "#",
+      imageSrc:
+        "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: "$35",
+      color: "Black",
+    },
+  ];
   return (
-    <div className="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
-          <img alt="ảnh" src="https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"  className="w-full rounded-lg sm:w-40" />
-          <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
-            <div className="mt-5 sm:mt-0">
-              <h2 className="text-lg font-bold text-gray-900">Nike Air Max 2019</h2>
-              <p className="mt-1 text-xs text-gray-700">36EU - 4US</p>
+    <div >
+      {productsCart.map((item) => (
+        <div key={item.id} className="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
+        <img alt={item.imageAlt} src={item.imageSrc}  className="w-full rounded-lg sm:w-40" />
+        <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
+          <div className="mt-5 sm:mt-0">
+            <h2 className="text-lg font-bold text-gray-900">{item.name}</h2>
+            <p className="mt-1 text-xs text-gray-700">36EU - 4US</p>
+          </div>
+          <div className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
+            <div className="flex items-center border-gray-100">
+              <span className="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50"> - </span>
+              <input className="h-8 w-8 border bg-white text-center text-xs outline-none" type="number" value="2" min="1" />
+              <span className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50"> + </span>
             </div>
-            <div className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
-              <div className="flex items-center border-gray-100">
-                <span className="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50"> - </span>
-                <input className="h-8 w-8 border bg-white text-center text-xs outline-none" type="number" value="2" min="1" />
-                <span className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50"> + </span>
-              </div>
-              <div className="flex items-center space-x-4">
-                <p className="text-sm">259.000 ₭</p>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"  stroke="currentColor" className="h-5 w-5 cursor-pointer duration-150 hover:text-red-500">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                </svg>
-              </div>
+            <div className="flex items-center space-x-4">
+              <p className="text-sm">259.000 ₭</p>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"  stroke="currentColor" className="h-5 w-5 cursor-pointer duration-150 hover:text-red-500">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+              </svg>
             </div>
           </div>
         </div>
-  )
-}
+      </div>
+      ))}
+        
+    </div>
+  );
+};
 
-export default ItemProductCart
+export default ItemProductCart;
--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -3,7 +3,7 @@ import ItemProductCart from '../../components/card/ItemProductCart'
 
 const Cart = () => {
   return (
-    <div className="h-screen bg-gray-100 pt-20">
+    <div className=" bg-gray-100 pt-20">
     <h1 className="mb-10 text-center text-2xl font-bold">Giỏ Hàng</h1>
     <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
       <div className="rounded-lg md:w-2/3">
